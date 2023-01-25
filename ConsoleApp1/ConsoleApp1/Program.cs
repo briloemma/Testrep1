41eda38a2432fda7e1d6145cbb3f82f5ab76792b
unknown <briloemma@gmail.com>
2023-01-25 23:43:21 +0300

    First commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class TestClass
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello");
+    }
+}
